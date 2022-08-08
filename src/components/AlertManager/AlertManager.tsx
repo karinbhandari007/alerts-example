9b2ce20edeb6f36fd6fbe3f7d747264a7dfd258f
= <=>
2022-08-08 23:01:13 +0545

    Chore: comments added

--- src/components/AlertManager/AlertManager.tsx
+++ src/components/AlertManager/AlertManager.tsx
@@ -8,23 +8,35 @@ import React, {
   useCallback,
   useEffect,
 } from "react";
+/**
+ * MUI components
+ */
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
 import AlertTitle from "@mui/material/AlertTitle";
+import { Link, Typography } from "@mui/material";
+/**
+ * Redux
+ */
 import { Alert as AlertType, closeAlert } from "../../redux/alert/alert.slice";
 import { alertSelector } from "../../redux/alert/alert.selectors";
 import { useAppDispatch, useAppSelector } from "../../hooks/store";
-import { Link, Typography } from "@mui/material";
 
 const MuiAlert = forwardRef<HTMLDivElement, any>((props, ref) => {
   const { id, text, title, timeLimit, link } = props;
   const dispatch = useAppDispatch();
 
+  /**
+   * @description closes active alert component based on provided id
+   */
   const _handleClose = useCallback(
     () => dispatch(closeAlert({ alertId: id })),
     [id, dispatch]
   );
 
+  /**
+   * @returns JSX element (Alert details)
+   */
   const AlertChildren = () => (
     <>
       {title.length ? <AlertTitle>{title}</AlertTitle> : <React.Fragment />}
@@ -38,6 +50,9 @@ const MuiAlert = forwardRef<HTMLDivElement, any>((props, ref) => {
     </>
   );
 
+  /**
+   * @description closes alert component after provided time limit
+   */
   useEffect(() => {
     setTimeout(() => {
       _handleClose();
@@ -74,10 +89,16 @@ const MuiAlert = forwardRef<HTMLDivElement, any>((props, ref) => {
 const AlertManager: FunctionComponent = (): ReactElement => {
   const alerts = useAppSelector(alertSelector);
 
+  /**
+   * @description if no alerts return empty component
+   */
   if (!alerts.length) {
     return <React.Fragment />;
   }
 
+  /**
+   * @description if alerts on store loop through it and display alerts on right top
+   */
   return (
     <Stack sx={{ position: "absolute", top: 10, right: 10 }} spacing={2}>
       {alerts.map((alert: AlertType) => {
